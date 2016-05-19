17c005d8db26fb062cee1971919b2eeb9c25deab
Sabarish Raghu <sabarish.raghuram@gmail.com>
2016-05-19 12:07:03 -0700

    Added Project Struct and Read Files

---
+++ src/core/CommandsProcessor.java
@@ -0,0 +1,9 @@
+package core;
+
+public class CommandsProcessor {
+
+	public CommandsProcessor() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/core/LensMain.java
@@ -0,0 +1,17 @@
+package core;
+
+public class LensMain {
+
+	public LensMain() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String args[])
+	{
+		while(true)
+		{
+			System.out.println("Lens>> ");
+		}
+	}
+
+}
